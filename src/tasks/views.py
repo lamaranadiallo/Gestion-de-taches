d01480fca4917b8d898f1ec1c99c80c565309f0e
Lamarana <mrdiallo77@gmail.com>
2024-07-23 13:36:37 +0200

    fix linting

--- src/tasks/views.py
+++ src/tasks/views.py
@@ -33,18 +33,18 @@ def task_list(request):
     if completed:
         tasks = tasks.filter(completed=completed == 'True')
 
-    return  render(request,'tasks/task_list.html',{'tasks':tasks})
+    return render(request, 'tasks/task_list.html', {'tasks' :tasks})
 
 
 @login_required
 def task_detail(request, pk):
     tasks = get_object_or_404(Task, pk=pk, user=request.user)
-    return render(request, 'tasks/task_detail.html',{'tasks':tasks})
+    return render(request, 'tasks/task_detail.html', {'tasks' :tasks})
 
 
 @login_required
 def task_create(request):
-    if request.method =='POST':
+    if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
             task = form.save(commit=False)
@@ -53,26 +53,26 @@ def task_create(request):
             return redirect('task_detail', pk=task.pk)
         else:
             form = TaskForm()
-        return render(request, 'tasks/task_form.html',{'form':form})
+        return render(request, 'tasks/task_form.html', {'form' :form})
 
 
 @login_required
 def task_update(request, pk):
-    task = get_object_or_404(Task,pk=pk,user=request.user)
-    if request.method=='POST':
+    task = get_object_or_404(Task, pk=pk, user=request.user)
+    if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
             return redirect('task_detail', pk=task.pk)
         else:
-            form=TaskForm(instance=task)
-        return render(request,'tasks/task_form.html',{'form':form})
+            form = TaskForm(instance=task)
+        return render(request, 'tasks/task_form.html', {'form':form})
 
 
 @login_required
 def task_delete(request, pk):
-    task = get_object_or_404(Task,pk=pk, user=request.user)
-    if request.method =='POST':
+    task = get_object_or_404(Task, pk=pk, user=request.user)
+    if request.method == 'POST':
         task.delete()
-        return  redirect('task_list')
-    return render(request, 'tasks/task_confirm_delete.html',{'task':task})
+        return redirect('task_list')
+    return render(request, 'tasks/task_confirm_delete.html', {'task' :task})
